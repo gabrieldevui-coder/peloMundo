6438f1fe0bf59305d9af983e34cd9befdd82f7a3
Gabriel França <gabrieldevui@gmail.com>
2026-02-03 00:00:19 -0300

    chore(bootstrap): initial project structure

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unsafe-finally': 'off',
+    'no-unused-vars': 'off',
+    'react/jsx-key': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
