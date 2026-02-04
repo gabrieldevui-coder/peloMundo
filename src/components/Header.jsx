6fe2b62092e3be85278ad48f06d1752c80da702f
Gabriel França <gabrieldevui@gmail.com>
2026-02-04 09:18:58 -0300

    feat(header): add initial header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { NavLink } from 'react-router-dom';
+import Logo from '../assets/logo.svg?react';
+import UserIcon from '../assets/user-line.svg?react';
+
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
+    <header className={styles.header}>
+      <nav className="container">
+        <ul className={styles.navigate}>
           <li>
-            <NavLink to="/" end>
-              Inicio
+            <NavLink to="/" end aria-label="peloMundo - Início">
+              <Logo />
             </NavLink>
           </li>
           <li>
-            <NavLink to="/login">Entrar / Cadastrar</NavLink>
+            <NavLink className={styles.login} to="/login">
+              <span>Entrar / Cadastrar</span>
+              <UserIcon style={{ with: '20px', height: '20px' }} />
+            </NavLink>
           </li>
         </ul>
       </nav>
