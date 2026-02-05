aecbc9c947f2083c1eb69e57b5a14660b28f759f
Gabriel França <gabrieldevui@gmail.com>
2026-02-05 07:25:53 -0300

    feat(form): add useForm validation hook

---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const types = {
+  email: {
+    regex: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+    message: 'Preencha um email valido',
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(null);
+
+  function validate(value) {
+    if (type === false) return true;
+
+    if (value.length === 0) {
+      setError('Preencha o campo');
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError('Coloque um email valido');
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+  function onChange({ target }) {
+    setValue(target.value);
+    if (error) {
+      validate(target.value);
+    }
+  }
+
+  return {
+    value,
+    error,
+    onChange,
+    validate: ({ target }) => validate(target.value),
+    onBlur: ({ target }) => validate(target.value),
+  };
+};
+
+export default useForm;
