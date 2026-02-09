79530e21d4c84302aab86e5662070db3fc734b38
Gabriel França <gabrieldevui@gmail.com>
2026-02-09 09:52:17 -0300

    feat(app): configure routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,30 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
-import Login from './components/user/Login';
+import Login from './components/auth/Login';
+import { UserStorage } from './UserContext';
+import User from './components/auth/user/User';
+import ProtectRouter from './components/helper/ProtectRouter';
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" index element={<Home />} />
-          <Route path="/login" index element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" index element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+            <Route
+              path="/conta/*"
+              element={
+                <ProtectRouter>
+                  <User />
+                </ProtectRouter>
+              }
+            />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
