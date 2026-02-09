8d35eb00a8f71273bc5d3f45195ad10d364654f5
Gabriel França <gabrieldevui@gmail.com>
2026-02-09 09:29:24 -0300

    feat(ui): add header and title components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,12 @@ import styles from './Header.module.css';
 import { NavLink } from 'react-router-dom';
 import Logo from '../assets/logo.svg?react';
 import UserIcon from '../assets/user-line.svg?react';
-
+import { dataUser } from '../UserContext';
 const Header = () => {
+  const { data, userLogout } = React.useContext(dataUser);
+  function handleClick() {
+    console.log('clicou');
+  }
   return (
     <header className={styles.header}>
       <nav className="container">
@@ -14,11 +18,23 @@ const Header = () => {
               <Logo />
             </NavLink>
           </li>
+
           <li>
-            <NavLink className={styles.login} to="/login">
-              <span>Entrar / Cadastrar</span>
-              <UserIcon style={{ with: '20px', height: '20px' }} />
-            </NavLink>
+            <>
+              {data ? (
+                <div>
+                  <NavLink className={styles.login} to="/conta">
+                    <span>{data.name}</span>
+                    <UserIcon style={{ with: '20px', height: '20px' }} />
+                  </NavLink>
+                </div>
+              ) : (
+                <NavLink className={styles.login} to="/login">
+                  <span>Entrar / Cadastrar</span>
+                  <UserIcon style={{ with: '20px', height: '20px' }} />
+                </NavLink>
+              )}
+            </>
           </li>
         </ul>
       </nav>
